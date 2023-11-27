37197f1c7c70774233602d151a928d9f79fd2347
Samuel Cahyawijaya <scahyawijaya@connect.hk.ust>
2023-11-27 12:56:03 +0800

    add mabl & maps dataset

---
+++ evaluation/mabl_data/dowload_data.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/simran-khanuja/Multilingual-Fig-QA/main/langdata/su.csv
+wget https://raw.githubusercontent.com/simran-khanuja/Multilingual-Fig-QA/main/langdata/jv.csv
+wget https://raw.githubusercontent.com/simran-khanuja/Multilingual-Fig-QA/main/langdata/id.csv
