c059ae53ba94452fe0827046d9fe6b24be3065c8
Samuel Cahyawijaya <samuel.cahyawijaya@gmail.com>
2023-12-17 21:38:17 +0800

    update code for llama2

--- evaluation/main_nlu_prompt_batch.py
+++ evaluation/main_nlu_prompt_batch.py
@@ -126,10 +126,10 @@ if __name__ == '__main__':
         model = AutoModelForCausalLM.from_pretrained(MODEL, device_map="auto", load_in_8bit=True, trust_remote_code=True)
         model = PeftModel.from_pretrained(model, ADAPTER, torch_dtype=torch.float16)
         MODEL = ADAPTER # for file naming
-    elif "bloom" in MODEL or "xglm" in MODEL or "gpt2" in MODEL or "sealion7b" in MODEL or "Merak" in MODEL or "SeaLLM" in MODEL:
+    elif "bloom" in MODEL or "xglm" in MODEL or "gpt2" in MODEL or "sealion7b" in MODEL or "Merak" in MODEL or "SeaLLM" in MODEL or  "Llama" in MODEL:
         model = AutoModelForCausalLM.from_pretrained(MODEL, device_map="auto", load_in_8bit=True, trust_remote_code=True)
         if "sealion7b" in MODEL:
-            tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+            tokenizer.pad_token = tokenizer.eos_token # Use EOS to pad label
     else:
         model = AutoModelForSeq2SeqLM.from_pretrained(MODEL, device_map="auto", load_in_8bit=True, trust_remote_code=True)
         tokenizer.pad_token = tokenizer.eos_token # Use EOS to pad label
